e0108177f98fd37040888b7587f2c78393c2c336
wataru okada <wataru.okada@flect.co.jp>
2020-10-03 10:40:49 +0900

    little update

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,8 @@ import { Coords3D } from '@tensorflow-models/facemesh/dist/util';
 import { TRIANGULATION } from './traiangulation';
 import { Icon, Label, Dropdown } from 'semantic-ui-react';
 import { init_facemesh_render, draw_facemesh_tri_tex2 } from './ShaderUtil';
+import { getDeviceLists, getVideoDevice } from './CameraUtil';
 
-
-export const getVideoDevice = async (deviceId:string): Promise<MediaStream|null>=>{
-
-  const webCamPromise = navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: { deviceId: deviceId,
-          width: { ideal: 1280 },
-          height: { ideal: 720 }
-      }
-  })
-  return webCamPromise
-}
-export const getDeviceLists = async () =>{
-  const list = await navigator.mediaDevices.enumerateDevices()
-  console.log("GET_DEVICE_LIST", list)
-
-  const audioInputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
-      return x.kind === "audioinput"
-  })
-  const videoInputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
-      return x.kind === "videoinput"
-  })
-  const audioOutputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
-      return x.kind === "audiooutput"
-  })
-  const videoInputResolutions = [
-      {deviceId: "360p", groupId: "360p", kind: "videoinputres", label: "360p"},
-      {deviceId: "540p", groupId: "540p", kind: "videoinputres", label: "540p"},
-      {deviceId: "720p", groupId: "720p", kind: "videoinputres", label: "720p"},
-  ]
-  return{
-      audioinput    : audioInputDevices,
-      videoinput    : videoInputDevices,
-      audiooutput   : audioOutputDevices,
-      videoinputres : videoInputResolutions,
-  }
-}
 
 class App extends React.Component {
   imageElementRef = React.createRef<HTMLImageElement>()
---
+++ src/CameraUtil.ts
@@ -0,0 +1,43 @@
+
+
+
+export const getVideoDevice = async (deviceId:string): Promise<MediaStream|null>=>{
+
+    const webCamPromise = navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { deviceId: deviceId,
+            width: { ideal: 1280 },
+            height: { ideal: 720 }
+        }
+    })
+    return webCamPromise
+ 
+}
+
+
+export const getDeviceLists = async () =>{
+    const list = await navigator.mediaDevices.enumerateDevices()
+    console.log("GET_DEVICE_LIST", list)
+  
+    const audioInputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
+        return x.kind === "audioinput"
+    })
+    const videoInputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
+        return x.kind === "videoinput"
+    })
+    const audioOutputDevices = list.filter((x:InputDeviceInfo | MediaDeviceInfo)=>{
+        return x.kind === "audiooutput"
+    })
+    const videoInputResolutions = [
+        {deviceId: "360p", groupId: "360p", kind: "videoinputres", label: "360p"},
+        {deviceId: "540p", groupId: "540p", kind: "videoinputres", label: "540p"},
+        {deviceId: "720p", groupId: "720p", kind: "videoinputres", label: "720p"},
+    ]
+    return{
+        audioinput    : audioInputDevices,
+        videoinput    : videoInputDevices,
+        audiooutput   : audioOutputDevices,
+        videoinputres : videoInputResolutions,
+    }
+}
+  
